fc75de030d60ca65c82d83873a1dff0aca2983c2
Jeonghoon Lee <neogia@naver.com>
2025-08-18 19:00:23 +0900

    Update LetingAdRemove.user.js

--- LetingAdRemove.user.js
+++ LetingAdRemove.user.js
@@ -3,16 +3,20 @@
 // @version      1.0
 // @description  Leting AD Remove
 // @match        *://leting.kr/*
-// @run-at       document-start
+// @run-at       document-end
 // ==/UserScript==
-
-(function() {
-  'use strict';  
-	for (i=0; i<document.forms.length; i++) {
-		for (k=0; k<document.forms[i].length; k++) {
-			el = document.forms[i].elements[k];    
-			if (el.type == "select-one" && el.style.visibility == 'hidden')
-				el.style.visibility = 'visible';
-		}
-	}
+(function () {
+    'use strict';
+    // <div class="contentsBox"> 의 **직접 자식** 중 class 에 'view-padding' 이 포함된 요소 찾기
+    const targets = document.querySelectorAll('div.contentsBox > div.cropped');
+    targets.forEach(el = > {
+        // "cropped" 클래스가 있으면 제거
+        if (el.classList.contains('cropped')) {
+            el.classList.remove('cropped');
+        }
+    });
+    const ads = document.querySelectorAll('div.adButton');
+    ads.forEach(el = > {
+        el.style.display = 'none';
+    });
 })();
